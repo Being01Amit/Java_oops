e0c968b68a7974e3bd4bf24ac08c195298c22beb
Amit <amitsunar54321@gmail.com>
2021-11-26 21:43:07 +0530

    Commit 1.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        int[] arr= {21,32,123,2,321,23,2};
+
+        for(int i = 0; i < arr.length -1; i++){
+            System.out.println(arr[i]);
+        }
+
     }
 }
 
