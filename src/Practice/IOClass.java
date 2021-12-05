be0f600a35ff2cf9f50ca0e992b4e64420da5263
being01amit <amitsunar54321@gmail.com>
2021-12-05 13:11:45 +0530

    Linux 1.3.3 "Implementing the IO Lib and doing Copying the file function"

--- src/Practice/IOClass.java
+++ src/Practice/IOClass.java
@@ -9,6 +9,7 @@ public class IOClass {
         // here we are callling the Buffer Writer object and do some write function on it
         bw.write("Amit ");
         bw.write("Akshit Sharma");
+        bw.write("Anuj");
 
         // CLosing the buffer Writer so this object no longer take input or write function
         bw.close();
@@ -24,6 +25,11 @@ public class IOClass {
         BufferedWriter br1 = new BufferedWriter(
                 new FileWriter("/home/being01amit/Desktop/Test Inputs/Output-copy.txt/")
         );
+/* Copying file in new File using the readLine function  */
+        String cf;
+        while ((cf = br.readLine()) != null){
+            br1.write(cf);
+        }
 
 
 /* In this Line of the Code we are storing the Buffer Reader input into String variable and printing them accordingly   */
